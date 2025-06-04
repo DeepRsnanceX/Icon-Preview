0a9baa230fffa97051b8d67fbaa4825682d0e180
Saritahhh <tusmuertos018@gmail.com>
2025-06-03 19:30:23 -0600

    wip glow fix !!!

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,18 @@
 
 using namespace geode::prelude;
 
+auto doGlowFix = Mod::get()->getSettingValue<bool>("glow-fix");
+
+$on_mod(Loaded) {
+    listenForSettingChanges("glow-fix", [](bool value) {
+        doGlowFix = value;
+    });
+}
+
 class $modify(IPGarageLayer, GJGarageLayer){
     struct Fields {
         SimplePlayer* m_previewPlayer = SimplePlayer::create(0);
+        SimplePlayer* fakeGlowDisplay = SimplePlayer::create(0);
     };
 
     static void onModify(auto& self) {
@@ -52,7 +61,54 @@ class $modify(IPGarageLayer, GJGarageLayer){
         if (!gameManager->getPlayerGlow()) {
             fields->m_previewPlayer->disableGlowOutline();
         }
-        
+
+        if (doGlowFix) {
+            fields->fakeGlowDisplay->m_firstLayer->setVisible(false);
+            fields->fakeGlowDisplay->m_secondLayer->setVisible(false);
+            fields->fakeGlowDisplay->m_birdDome->setVisible(false);
+            fields->fakeGlowDisplay->m_detailSprite->setVisible(false);
+
+            fields->fakeGlowDisplay->setPosition({posX, posY});
+            fields->fakeGlowDisplay->setScale(1.6f);
+
+            if (lastSelectedIcon == IconType::Ship || lastSelectedIcon == IconType::Ufo) {
+                if (gameManager->getPlayerGlow()) {
+                    fields->fakeGlowDisplay->setVisible(true);
+                    fields->fakeGlowDisplay->m_outlineSprite->setVisible(true);
+                    m_playerObject->m_outlineSprite->setVisible(false);
+                } else {
+                    fields->fakeGlowDisplay->setVisible(false);
+                }
+            }
+
+            if (lastSelectedIcon == IconType::Ship) {
+                fields->fakeGlowDisplay->updatePlayerFrame(gameManager->getPlayerShip(), IconType::Ship);
+                MoreIcons::updateSimplePlayer(fields->fakeGlowDisplay, IconType::Ship);
+                fields->fakeGlowDisplay->setColor(gameManager->colorForIdx(gameManager->getPlayerColor()));
+                fields->fakeGlowDisplay->setSecondColor(gameManager->colorForIdx(gameManager->getPlayerColor2()));
+                fields->fakeGlowDisplay->setGlowOutline(gameManager->colorForIdx(gameManager->getPlayerGlowColor()));
+                fields->fakeGlowDisplay->enableCustomGlowColor(gameManager->colorForIdx(gameManager->getPlayerGlowColor()));
+                if (!gameManager->getPlayerGlow()) {
+                    fields->fakeGlowDisplay->disableGlowOutline();
+                }
+            } else if (lastSelectedIcon == IconType::Ufo) {
+                fields->fakeGlowDisplay->updatePlayerFrame(gameManager->getPlayerBird(), IconType::Ufo);
+                MoreIcons::updateSimplePlayer(fields->fakeGlowDisplay, IconType::Ufo);
+                fields->fakeGlowDisplay->setColor(gameManager->colorForIdx(gameManager->getPlayerColor()));
+                fields->fakeGlowDisplay->setSecondColor(gameManager->colorForIdx(gameManager->getPlayerColor2()));
+                fields->fakeGlowDisplay->setGlowOutline(gameManager->colorForIdx(gameManager->getPlayerGlowColor()));
+                fields->fakeGlowDisplay->enableCustomGlowColor(gameManager->colorForIdx(gameManager->getPlayerGlowColor()));
+                if (!gameManager->getPlayerGlow()) {
+                    fields->fakeGlowDisplay->disableGlowOutline();
+                }
+            }
+            
+            fields->fakeGlowDisplay->setID("fake-glow-display"_spr);
+            this->addChild(fields->fakeGlowDisplay, -2);
+        }
+
+        fields->m_previewPlayer->setID("preview-simpleplayer"_spr);
+
         this->addChild(fields->m_previewPlayer, -1);
 
         return true;
@@ -63,23 +119,64 @@ class $modify(IPGarageLayer, GJGarageLayer){
 
         auto fields = m_fields.self();
         auto manager = GameManager::sharedState();
-        auto btn = static_cast<CCMenuItemSpriteExtra*>(iconSender);
-
-        if (m_selectedIconType == IconType::Cube) {
-            fields->m_previewPlayer->updatePlayerFrame(manager->getPlayerFrame(), IconType::Cube);
-            MoreIcons::updateSimplePlayer(fields->m_previewPlayer, IconType::Cube);
-            fields->m_previewPlayer->setColor(manager->colorForIdx(manager->getPlayerColor()));
-            fields->m_previewPlayer->setSecondColor(manager->colorForIdx(manager->getPlayerColor2()));
-            fields->m_previewPlayer->setGlowOutline(manager->colorForIdx(manager->getPlayerGlowColor()));
-            fields->m_previewPlayer->enableCustomGlowColor(manager->colorForIdx(manager->getPlayerGlowColor()));
-            if (!manager->getPlayerGlow()) {
-                fields->m_previewPlayer->disableGlowOutline();
-            }
-        }
 
         float globalPosX = m_playerObject->getPositionX();
+        float globalPosY = m_playerObject->getPositionY();
         float shipPosY = m_playerObject->getPositionY() + 16.f;
         float ufoPosY = m_playerObject->getPositionY() + 8.5f;
+
+        // this runs on selecting any mode just to make sure it updates properly
+        fields->m_previewPlayer->updatePlayerFrame(manager->getPlayerFrame(), IconType::Cube);
+        MoreIcons::updateSimplePlayer(fields->m_previewPlayer, IconType::Cube);
+        fields->m_previewPlayer->setColor(manager->colorForIdx(manager->getPlayerColor()));
+        fields->m_previewPlayer->setSecondColor(manager->colorForIdx(manager->getPlayerColor2()));
+        fields->m_previewPlayer->setGlowOutline(manager->colorForIdx(manager->getPlayerGlowColor()));
+        fields->m_previewPlayer->enableCustomGlowColor(manager->colorForIdx(manager->getPlayerGlowColor()));
+        if (!manager->getPlayerGlow()) {
+            fields->m_previewPlayer->disableGlowOutline();
+        }
+
+        if (doGlowFix) {
+            fields->fakeGlowDisplay->m_firstLayer->setVisible(false);
+            fields->fakeGlowDisplay->m_secondLayer->setVisible(false);
+            fields->fakeGlowDisplay->m_birdDome->setVisible(false);
+            fields->fakeGlowDisplay->m_detailSprite->setVisible(false);
+
+            if (m_selectedIconType == IconType::Ship || m_selectedIconType == IconType::Ufo) {
+                if (manager->getPlayerGlow()) {
+                    fields->fakeGlowDisplay->setVisible(true);
+                    fields->fakeGlowDisplay->m_outlineSprite->setVisible(true);
+                    m_playerObject->m_outlineSprite->setVisible(false);
+                }
+            } else {
+                fields->fakeGlowDisplay->setVisible(false);
+                m_playerObject->m_outlineSprite->setVisible(manager->getPlayerGlow());
+            }
+
+            if (m_selectedIconType == IconType::Ship) {
+                fields->fakeGlowDisplay->updatePlayerFrame(manager->getPlayerShip(), IconType::Ship);
+                MoreIcons::updateSimplePlayer(fields->fakeGlowDisplay, IconType::Ship);
+                fields->fakeGlowDisplay->setColor(manager->colorForIdx(manager->getPlayerColor()));
+                fields->fakeGlowDisplay->setSecondColor(manager->colorForIdx(manager->getPlayerColor2()));
+                fields->fakeGlowDisplay->setGlowOutline(manager->colorForIdx(manager->getPlayerGlowColor()));
+                fields->fakeGlowDisplay->enableCustomGlowColor(manager->colorForIdx(manager->getPlayerGlowColor()));
+                if (!manager->getPlayerGlow()) {
+                    fields->fakeGlowDisplay->disableGlowOutline();
+                }
+            } else if (m_selectedIconType == IconType::Ufo) {
+                fields->fakeGlowDisplay->updatePlayerFrame(manager->getPlayerBird(), IconType::Ufo);
+                MoreIcons::updateSimplePlayer(fields->fakeGlowDisplay, IconType::Ufo);
+                fields->fakeGlowDisplay->setColor(manager->colorForIdx(manager->getPlayerColor()));
+                fields->fakeGlowDisplay->setSecondColor(manager->colorForIdx(manager->getPlayerColor2()));
+                fields->fakeGlowDisplay->setGlowOutline(manager->colorForIdx(manager->getPlayerGlowColor()));
+                fields->fakeGlowDisplay->enableCustomGlowColor(manager->colorForIdx(manager->getPlayerGlowColor()));
+                if (!manager->getPlayerGlow()) {
+                    fields->fakeGlowDisplay->disableGlowOutline();
+                }
+            }
+
+        
+        }
 
         auto moveToShipPos = CCEaseBackInOut::create(
             CCMoveTo::create(0.3f, {globalPosX, shipPosY})
